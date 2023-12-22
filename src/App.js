2cdf9edf2785b56e007e938bd8062a169e93ed21
Gautham2980 <gautham4816@gmail.com>
2023-12-22 11:43:01 +0530

    apimethods

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Api from './Card';
+
+function App() {
+  return (
+    <div className="App">
+    <div>
+      <Api />
+
+    </div>
+      
+    </div>
+  );
+}
+
+export default App;
